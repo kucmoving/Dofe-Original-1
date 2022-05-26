b3f87e06bc8daef7729cabdd31776a962b81d585
kucmoving <kuc.moving@gmail.com>
2022-05-26 09:51:56 +0100

    Add project files.

---
+++ PetCafeCustomerWebApp/Controllers/DogController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetCafeCustomerWebApp.Data;
+using PetCafeCustomerWebApp.Models;
+namespace PetCafeCustomerWebApp.Controllers
+{
+    public class DogController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DogController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        //will go into applicationDbContext
+
+        public IActionResult Index()
+        {
+            List<Dog> dogs = _context.Dogs.ToList();
+            //Dogs the name of icollection of dog in customers
+            //tolist = to make table to be a list
+            return View(dogs);
+        }
+    }
+}
---
+++ PetCafeCustomerWebApp/Controllers/SharingController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetCafeCustomerWebApp.Data;
+using PetCafeCustomerWebApp.Models;
+namespace PetCafeCustomerWebApp.Controllers
+{
+    public class SharingController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SharingController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        //will go into applicationDbContext
+
+        public IActionResult Index()
+        {
+            List<Sharing> sharings = _context.Sharings.ToList();
+            //Dogs the name of icollection of dog in customers
+            //tolist = to make table to be a list
+            return View(sharings);
+        }
+    }
+}
